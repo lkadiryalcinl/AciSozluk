d46ba8a1ba072c83015936ebf6e4eeb643e7afee
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-09 20:40:09 +0300

    Create entry text area ui finished.

--- EksiSozluk.WebAPI/obj/Debug/net7.0/EksiSozluk.WebAPI.AssemblyInfo.cs
+++ EksiSozluk.WebAPI/obj/Debug/net7.0/EksiSozluk.WebAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EksiSozluk.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4b792440867b5db79f5f8b686a05527fe510a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e760f555b79209e6fe1f0d2489e54c4917d616b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EksiSozluk.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EksiSozluk.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
