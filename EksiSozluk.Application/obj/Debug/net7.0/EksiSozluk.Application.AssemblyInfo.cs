37b51cfe4f7baeb3ce2f43286d4217c8ed7679b3
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-11 22:47:50 +0300

    Favorites entry api consume opts

--- EksiSozluk.Application/obj/Debug/net7.0/EksiSozluk.Application.AssemblyInfo.cs
+++ EksiSozluk.Application/obj/Debug/net7.0/EksiSozluk.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EksiSozluk.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be79633ea57cd506b98e8fddef8982732f48c002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e448f0d09dd9517d0f87a81cb94d18648e4552")]
 [assembly: System.Reflection.AssemblyProductAttribute("EksiSozluk.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EksiSozluk.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
