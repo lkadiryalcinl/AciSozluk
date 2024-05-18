7c6ee0421e64c3b9ceaa75e513be9e5d15945d27
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-18 21:03:42 +0300

    ResultChannelDto created on WebUI

---
+++ EksiSozluk.WebUI/Dto/ChannelDtos/ResultChannelsDto.cs
@@ -0,0 +1,13 @@
+﻿using EksiSozluk.Domain.Entities;
+
+namespace EksiSozluk.WebUI.Dto.ChannelDtos
+{
+    public class ResultChannelsDto
+    {
+        public Guid Id { get; set; }
+
+        public string ChannelName { get; set; }
+
+        public string ChannelDescription { get; set; }
+    }
+}
