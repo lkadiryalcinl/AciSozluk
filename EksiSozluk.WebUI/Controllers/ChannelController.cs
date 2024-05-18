7018e0b6e4168d930954198ae8483a4906a6cf89
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-18 21:04:03 +0300

    ChannelController api consume

--- EksiSozluk.WebUI/Controllers/ChannelController.cs
+++ EksiSozluk.WebUI/Controllers/ChannelController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EksiSozluk.WebUI.Dto.ChannelDtos;
+using EksiSozluk.WebUI.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EksiSozluk.WebUI.Controllers
 {
     public class ChannelController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClientServiceAction _httpClientServiceAction;
+
+        public ChannelController(HttpClientServiceAction httpClientServiceAction)
         {
-            return View();
+            _httpClientServiceAction = httpClientServiceAction;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _httpClientServiceAction.InvokeAsync<List<ResultChannelsDto>>($"Channels/GetChannels");
+            return View(values);
         }
     }
 }
