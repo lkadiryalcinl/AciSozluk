f022978817141ad1a64f6892f0ad14d9428546f9
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-19 08:52:26 +0300

    FollowedDate prop added

--- EksiSozluk.Domain/Entities/FollowChannel.cs
+++ EksiSozluk.Domain/Entities/FollowChannel.cs
@@ -7,5 +7,6 @@
         public User User { get; set; }
         public Guid ChannelId { get; set; }
         public Channel Channel { get; set; }
+        public DateTime FollowedDate { get; set; }
     }
 }
