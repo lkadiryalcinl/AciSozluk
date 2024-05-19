e967d379de89639245418e31faf487d4bebac118
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-19 10:52:36 +0300

    Update ChannelController.cs

--- EksiSozluk.WebUI/Controllers/ChannelController.cs
+++ EksiSozluk.WebUI/Controllers/ChannelController.cs
@@ -1,6 +1,9 @@
 ﻿using EksiSozluk.WebUI.Dto.ChannelDtos;
+using EksiSozluk.WebUI.Dto.FollowChannelDtos;
 using EksiSozluk.WebUI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 
 namespace EksiSozluk.WebUI.Controllers
 {
@@ -15,8 +18,51 @@ namespace EksiSozluk.WebUI.Controllers
 
         public async Task<IActionResult> Index()
         {
+            var userId = HttpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var followedChannels = await _httpClientServiceAction.InvokeAsync<List<ResultFollowChannelDto>>($"FollowChannels/GetFollowChannelsByFilter?userId={userId}");
+
             var values = await _httpClientServiceAction.InvokeAsync<List<ResultChannelsDto>>($"Channels/GetChannels");
+
+            if (!userId.IsNullOrEmpty())
+            {
+                Parallel.ForEach(values, value =>
+            {
+                if (followedChannels.IsNullOrEmpty())
+                    return;
+                Parallel.ForEach(followedChannels, fc =>
+                {
+                    if (value.Id == fc.ChannelId)
+                        value.IsFollowedByUser = true;
+                });
+            });
+            }
             return View(values);
+        }
+
+        public async Task<IActionResult> UnfollowChannel(string channelId)
+        {
+            var userId = HttpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            await _httpClientServiceAction.RemoveAsync($"FollowChannels/UnfollowChannel?channelId={channelId}&userId={userId}");
+            return RedirectToAction("Index", "Channel");
+        }
+
+        public async Task<IActionResult> FollowChannel(string channelId)
+        {
+            var userId = HttpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!userId.IsNullOrEmpty())
+            {
+                CreateFollowChannelDto createFollowChannelDto = new()
+                {
+                    ChannelId = Guid.Parse(channelId),
+                    UserId = userId,
+                    FollowDate = DateTime.Now
+                };
+
+                await _httpClientServiceAction.CreateAsync<CreateFollowChannelDto>("FollowChannels/FollowChannel", createFollowChannelDto);
+
+                return RedirectToAction("Index", "Channel");
+            }
+            return RedirectToAction("Index", "Channel");
         }
     }
 }
