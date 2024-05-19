92fc63a8a4a96ce759bdf0b9c2a39f59cc2af84d
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-19 10:53:08 +0300

    FollowChannel Dtos in UI

---
+++ EksiSozluk.WebUI/Dto/FollowChannelDtos/CreateFollowChannelDto.cs
@@ -0,0 +1,11 @@
+﻿using EksiSozluk.Domain.Entities;
+
+namespace EksiSozluk.WebUI.Dto.FollowChannelDtos
+{
+    public class CreateFollowChannelDto
+    {
+        public string UserId { get; set; }
+        public Guid ChannelId { get; set; }
+        public DateTime FollowDate { get; set; }
+    }
+}
