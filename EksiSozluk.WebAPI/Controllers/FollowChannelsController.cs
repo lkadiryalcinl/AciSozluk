8af2c464fa939a7c512c481ada268d03e7da8214
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-05-19 08:50:56 +0300

    Create FollowChannelsController.cs
    FollowChannel Api Controller

---
+++ EksiSozluk.WebAPI/Controllers/FollowChannelsController.cs
@@ -0,0 +1,45 @@
+﻿using EksiSozluk.Application.Mediator.Commands.FollowChannelCommands;
+using EksiSozluk.Application.Mediator.Queries.FollowChannelQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EksiSozluk.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FollowChannelsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FollowChannelsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpPost("GetfollowChannelsByFilter")]
+        public async Task<IActionResult> UnfollowChannel(string UserId)
+        {
+            GetFollowChannelByFilterQuery query = new()
+            {
+                UserId = UserId
+            };
+            var values = await _mediator.Send(query);
+            return Ok(values);
+        }
+
+        [HttpPost("FollowChannel")]
+        public async Task<IActionResult> FollowChannel(CreateFollowChannelCommand createFollowChannelCommand)
+        {
+            await _mediator.Send(createFollowChannelCommand);
+            return Ok("Success");
+        }
+
+        [HttpPost("UnfollowChannel")]
+        public async Task<IActionResult> UnfollowChannel(RemoveFollowChannelCommand removeFollowChannelCommand)
+        {
+            await _mediator.Send(removeFollowChannelCommand);
+            return Ok("Success");
+        }
+    }
+}
